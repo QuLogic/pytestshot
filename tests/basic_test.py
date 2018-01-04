26879b4e0aa0f321e041c9c6563060dba43bfeb0
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2018-01-04 02:47:33 -0500

    Rewrite test without threading.

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -10,47 +10,30 @@ from gi.repository import Gtk
 import pytestshot
 
 
-class GtkWinInstance(object):
-    def start(self):
+class TestBasicWin(unittest.TestCase):
+    def setUp(self):
+        self.pil_img = None
+
         self.window = Gtk.Window()
         self.button = Gtk.Button.new_with_label("pouet")
         self.window.add(self.button)
         self.window.show_all()
         self.window.set_size_request(640, 480)
+        self.window.connect('destroy', Gtk.main_quit)
+        self.window.connect_after('draw', self.save_screenshot)
 
-        self.thread = threading.Thread(target=self._gtk_thread)
-        self.thread.start()
-
-        time.sleep(1)
-
-    def _gtk_thread(self):
-        Gtk.main()
-
-    def stop(self):
-        pytestshot.wait()
-        self.window.hide()
-        pytestshot.wait()
-        pytestshot.exit()
-        self.thread.join()
-
-
-class TestBasicWin(unittest.TestCase):
-    def setUp(self):
-        self.app = GtkWinInstance()
+    def save_screenshot(self, widget, cr):
+        assert widget == self.window
+        self.pil_img = pytestshot.screenshot(widget.get_window())
+        widget.destroy()
 
     def test_screenshot(self):
-        self.app.start()
-        try:
-            pil_img = pytestshot.screenshot(self.app.window.get_window())
-            self.assertNotEqual(pil_img, None)
-        finally:
-            self.app.stop()
+        Gtk.main()
+
+        self.assertNotEqual(self.pil_img, None)
 
     def test_ref(self):
-        self.app.start()
-        try:
-            pil_img = pytestshot.screenshot(self.app.window.get_window())
-            self.assertNotEqual(pil_img, None)
-        finally:
-            self.app.stop()
-        pytestshot.assertScreenshot(self, "test_basic", pil_img)
+        Gtk.main()
+
+        self.assertNotEqual(self.pil_img, None)
+        pytestshot.assertScreenshot(self, "test_basic", self.pil_img)
